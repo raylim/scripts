6b1cfc90eaee88c3bcca5a63c41ad9156f74fb8c
Raymond Lim <limr@unagi.cbio.mskcc.org>
2013-09-04 17:44:54 -0400

    update

--- chasmVcf.R
+++ chasmVcf.R
@@ -9,6 +9,7 @@ options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
 optList <- list(
                 make_option("--genome", default = 'hg19', help = "genome build [default %default]"),
                 make_option("--chasmDir", default = NULL, help = "CHASM dir"),
+                make_option("--pythonDir", default = NULL, help = "python directory"),
                 make_option("--classifier", default = 'Breast', help = "CHASM classifier [default %default]"),
                 make_option("--outFile", default = stdout(), help = "vcf output file [default %default]"))
 
@@ -20,15 +21,18 @@ if (is.null(opt$chasmDir)) {
     cat("Need CHASM dir\n");
     print_help(parser);
     stop();
+} else if (is.null(opt$pythonDir)) {
+    cat("Need python dir\n");
+    print_help(parser);
+    stop();
 } else if (length(arguments$args) < 1) {
-    cat("Reading from stdin...\n");
+    #cat("Reading from stdin...\n");
     tmp <- tempfile()
     f <- file('stdin')
     open(f)
     x <- readLines(f)
     close(f)
-    write(x, file = 'x.vcf') 
-    cat("wrote temp file to ", tmp, "\n");
+    write(x, file = tmp)
     vcf <- readVcf(tmp, genome = opt$genome)
 } else {
     fn <- arguments$args[1];
@@ -36,16 +40,21 @@ if (is.null(opt$chasmDir)) {
 }
 
 al <- sapply(rowData(vcf)$ALT, length)
-X <- cbind(seq = as.character(seqnames(rowData(vcf))), start = start(rowData(vcf)), end = start(rowData(vcf)) + width(rowData(vcf)), strand = rep('+', nrow(vcf)), ref = as.character(rowData(vcf)$REF))
+X <- cbind(id = 1:nrow(vcf), seq = as.character(seqnames(rowData(vcf))), zero = start(rowData(vcf)) - 1, one = start(rowData(vcf)) + 1, strand = rep('+', nrow(vcf)), ref = as.character(rowData(vcf)$REF), filt = rowData(vcf)$FILTER)
 XX <- apply(X, 2, rep, times = al)
 X <- cbind(XX, alt = as.character(unlist(rowData(vcf)$ALT)))
+X <- as.data.frame(X, stringsAsFactors = F)
+X <- subset(X, sapply(alt, nchar) == 1 & sapply(ref, nchar) == 1 & filt == "PASS")
+X <- X[,-which(colnames(X) == 'filt')]
+X$seq <- sub('^', 'chr', X$seq)
 
 setwd(opt$chasmDir)
 tmp <- tempfile()
 write.table(X, file = tmp, quote = F, sep = '\t', row.names = F, col.names = F)
-cmd <- paste("CHASMDIR=", opt$chasmDir, " ./RunChasm ", opt$classifier, ' ', tmp, ' -g' ,sep = '')
+cmd <- paste("CHASMDIR=", opt$chasmDir, " PATH=", opt$pythonDir, "/bin ./RunChasm ", opt$classifier, ' ', tmp, ' -g' ,sep = '')
+#cat(cmd)
 system(cmd)
-results <- read.table(file = paste(tmp, '.output', sep = ''), sep = '\t', header = T, as.is = T)
+results <- read.table(file = paste(tmp, opt$classifier, '.output', sep = ''), sep = '\t', header = T, as.is = T)
 
 hinfoprime <- apply(as.data.frame(info(header(vcf))), 2, as.character)
 rownames(hinfoprime) <- rownames(info(header(vcf)))
@@ -54,14 +63,19 @@ hinfoprime <- rbind(hinfoprime, chasm_pval = c("A", "Float", "CHASM p-value"))
 hinfoprime <- rbind(hinfoprime, chasm_score = c("A", "Float", "CHASM score"))
 hinfoprime <- rbind(hinfoprime, chasm_fdr = c("A", "Float", "CHASM B-H FDR"))
 hinfoprime <- DataFrame(hinfoprime)
-exptData(vcf)$header <- VCFHeader(samples = header(vcf)@samples, meta = meta(header(vcf)), info = hinfoprime, geno = geno(header(vcf)))
+hlist <- header(exptData(vcf)$header)
+hlist$INFO <- hinfoprime
+exptData(vcf)$header <- new("VCFHeader", samples = header(vcf)@samples, header = hlist)
 
-infoprime <- info(vcf)
-infoprime[as.integer(results$MutationID),"chasm_mut"] <- as.character(results$Mutation)
-infoprime[as.integer(results$MutationID),"chasm_score"] <- results$CHASM
-infoprime[as.integer(results$MutationID),"chasm_pval"] <- results$PValue
-infoprime[as.integer(results$MutationID),"chasm_fdr"] <- results$BHFDR
-info(vcf) <- infoprime
+if (nrow(results) > 1) {
+    infoprime <- info(vcf)
+    infoprime[as.integer(results$MutationID),"chasm_mut"] <- as.character(results$Mutation)
+    infoprime[as.integer(results$MutationID),"chasm_score"] <- results$CHASM
+    infoprime[as.integer(results$MutationID),"chasm_pval"] <- results$PValue
+    if (nrow(results) > 10) {
+        infoprime[as.integer(results$MutationID),"chasm_fdr"] <- results$BHFDR
+    }
+    info(vcf) <- infoprime
+}
 
 writeVcf(vcf, opt$outFile)
-

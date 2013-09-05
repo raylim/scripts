595c1dbac558bd172d36c8953b57160462b8cc57
raylim <limr@mskcc.org>
2013-09-05 19:37:15 -0400

    fix typo

--- extractCoordsFromDefuse.pl
+++ extractCoordsFromDefuse.pl
@@ -15,18 +15,14 @@ while (my $line = <>) {
     my @arr = split /\t/, $line;
     my %F = map { $_ => shift @arr } @header;
 
-    print "gene1: " . $F{gene_name1} . "\n";
-    print "gene2: " . $F{gene_name2} . "\n";
-    print "upstream gene: " . $F{upstream_gene} . "\n";
-    print "downstream gene: " . $F{downstream_gene} . "\n";
     my $upstream = ($F{upstream_gene} eq $F{gene_name1})? 1 : 2;
     my $downstream = ($F{downstream_gene} eq $F{gene_name1})? 1 : 2;
 
     my $upstreamChr = $F{"gene_chromosome" . $upstream };
-    my $downstreamChr = $F{"gene_chromosome" . $upstream };
+    my $downstreamChr = $F{"gene_chromosome" . $downstream };
 
     my $upstreamPosn = $F{"genomic_break_pos" . $upstream };
-    my $downstreamPosn = $F{"genomic_break_pos" . $upstream };
+    my $downstreamPosn = $F{"genomic_break_pos" . $downstream };
 
     print join("\t", ($upstreamChr, $upstreamPosn, $downstreamChr, $downstreamPosn)) . "\n";
 }

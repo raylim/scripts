8d0fbda77ffa1205df823ee8c9dcf5498614af01
raylim <limr@mskcc.org>
2013-11-25 13:12:13 -0500

    update

--- tophatFusionOncofuse.R
+++ tophatFusionOncofuse.R
@@ -22,6 +22,7 @@ tx1 <- transcriptsByOverlaps(txdb, gr1)
 olaps <- findOverlaps(tx1, gr1)
 X <- tx1[queryHits(olaps)]
 X$id <- subjectHits(olaps)
+X$pos <- start(gr1)[subjectHits(olaps)]
 tx1 <- X
 #tx1$id[ <- subjectHits(findOverlaps(tx1, gr1))
 egids <- mget(tx1$tx_name, org.Hs.egENSEMBLTRANS2EG, ifnotfound = NA)
@@ -44,6 +45,7 @@ tx2 <- transcriptsByOverlaps(txdb, gr2)
 olaps <- findOverlaps(tx2, gr2)
 X <- tx2[queryHits(olaps)]
 X$id <- subjectHits(olaps)
+X$pos <- start(gr2)[subjectHits(olaps)]
 tx2 <- X
 egids <- mget(tx2$tx_name, org.Hs.egENSEMBLTRANS2EG, ifnotfound = NA)
 egids <- sapply(egids, function (x) x[1])
@@ -64,4 +66,12 @@ XX <- as.data.frame(tx1)
 YY <- as.data.frame(tx2)
 Data <- merge(XX, YY, by = "id", suffixes = c(".1", ".2"))
 Data <- subset(Data, genepos.1 != genepos.2)
+Data <- transform(Data, upstreamChrom = ifelse(genepos.1 == "5prime", seqnames.1, seqnames.2))
+Data <- transform(Data, upstreamPos = ifelse(genepos.1 == "5prime", pos.1, pos.2))
+Data <- transform(Data, downstreamChrom = ifelse(genepos.1 != "5prime", seqnames.1, seqnames.2))
+Data <- transform(Data, downstreamPos = ifelse(genepos.1 != "5prime", pos.1, pos.2))
 
+oncofuseCoords <- Data[, c("upstreamChrom", "upstreamPos", "downstreamChrom", "downstreamPos")]
+if (any(duplicated(oncofuseCoords))) {
+    oncofuseCoords <- oncofuseCoords[!duplicated(oncofuseCoords), ]
+}

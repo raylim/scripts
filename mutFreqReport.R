f7bab7af9659f70eae03439d78586d743c611c33
raylim <raylim@mm.st>
2013-12-05 15:45:31 -0500

    mutation freq report

---
+++ mutFreqReport.R
@@ -0,0 +1,79 @@
+#!/usr/bin/env Rscript
+# plot nucleotide frequency using mutect input
+
+suppressPackageStartupMessages(library("optparse"));
+suppressPackageStartupMessages(library("hwriter"));
+
+options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
+
+optList <- list(
+                make_option("--outDir", default = NULL, type = "character", action = "store", help = "Output directory (required)"))
+
+parser <- OptionParser(usage = "%prog [options]", option_list = optList);
+arguments <- parse_args(parser, positional_arguments = T);
+opt <- arguments$options;
+
+if (length(arguments$args) < 1) {
+    cat("Need mutect results table(s)\n");
+    print_help(parser);
+    stop();
+} else if (is.null(opt$outDir)) {
+    cat("Need output dir\n\n");
+    print_help(parser);
+    stop();
+} else {
+    mutectFiles <- arguments$args;
+}
+
+
+d <- list()
+for (mutectFile in mutectFiles) {
+    cat("Reading", mutectFile, "\n")
+    sp <- sub('.*/', '', mutectFile)
+    sp <- sub('\\..*', '', sp)
+    d[[sp]] <- read.table(mutectFile, header=T, as.is = T, sep = '\t');
+    d[[sp]] <- subset(d[[sp]], judgement == "KEEP")
+}
+dd <- do.call('rbind', d)
+
+pg <- openPage('index.html', dirname = opt$outDir)
+
+# mosaic plots for each conteingcy table
+cols <- c("blue", "red", "green", "yellow")
+
+conTable <- table(ref = dd$ref_allele, alt = dd$alt_allele)
+fn <- paste(opt$outDir, '/all.mosaicplot.png', sep = '')
+png(fn, height = 500, width = 500)
+mosaicplot(conTable, main = "All", col = cols)
+dev.off()
+hwriteImage(basename(fn), pg)
+x <- as.matrix(conTable)
+class(x) <- 'matrix'
+hwrite(x, pg, br = T)
+
+conTables <- lapply(d, function(x) table(ref = x$ref_allele, alt = x$alt_allele))
+dfs <- lapply(conTables, as.data.frame)
+df <- dfs[[1]][, c(1, 2)]
+for (sp in names(dfs)) {
+    x <- dfs[[sp]]
+    colnames(x)[3] <- sp
+    df <- merge(df, x, by = c("ref", "alt"))
+}
+hwrite(df, pg, br = T)
+fn <- paste(opt$outDir, '/refvarCount.txt', sep = '')
+write.table(df, file = fn, sep = '\t', quote = F)
+
+
+for (sp in names(conTables)) {
+    tab <- conTables[[sp]]
+    fn <- paste(opt$outDir, '/', sp, '.mosaicplot.png', sep = '')
+    png(fn, height = 400, width = 400)
+    mosaicplot(tab, col = cols, main = sp)
+    dev.off()
+    hwriteImage(basename(fn), pg, br = F)
+    x <- as.matrix(tab)
+    class(x) <- 'matrix'
+    hwrite(x, pg, br = T)
+}
+
+closePage(pg)

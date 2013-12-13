dc9e506ad23aae8b0122c537f481f367b2f14496
raylim <raylim@mm.st>
2013-12-13 17:53:04 -0500

    optparse

--- hapseg.R
+++ hapseg.R
@@ -1,51 +1,71 @@
-## default values
-plate.name <- "Plate"
-snp.fn <- "apt/quant-norm.pm-only.med-polish.expr.summary.txt"
-genome.build <-  "hg19"
-phased.bgl.dir <- "/home/ngk1/reference/phasedBGL"
-calls.fn <- "apt/birdseed-v1.calls.txt"
-clusters.fn <- "apt/birdseed-v1.snp-models.txt"
-genome.build <- "hg19"
-disease <- "breastcancer"
-platform <- "SNP_6.0"
-tumour <- ""
-normal <- ""
-results.dir <- ""
+#!/usr/bin/env Rscript
+# Run absolute
 
-args <- (commandArgs(TRUE))
+suppressPackageStartupMessages(library("optparse"));
+suppressPackageStartupMessages(library(HAPSEG));
 
-if (length(args) == 0) {
-	print("No arguments supplied.\n")
-    exit(1)
+options(warn = -1, error = quote({ traceback(2); q('no', status = 1) }))
+
+optList <- list(
+        make_option("--disease", default = 'breastcancer', help = "disease [default %default]"),
+        make_option("--platform", default = "SNP_6.0", help = "platform [default %default]"),
+        make_option("--plate", default = 'Plate', help = "Plate name [default %default]"),
+        make_option("--callsFile", default = NULL, help = "birdseed calls file"),
+        make_option("--summaryFile", default = NULL, help = "APT summary file"),
+        make_option("--clustersFile", default = NULL, help = "birdseed models file"),
+        make_option("--ref", default = 'hg19', help = "genome build [default %default]"),
+        make_option("--resultsDir", default = NULL, help = "results directory"),
+        make_option("--phasedBGLDir", default = NULL, help = "Phased BGL directory"),
+        make_option("--outFile", default = NULL, help = "output file")
+        )
+parser <- OptionParser(usage = "%prog tumour [normal]", option_list = optList);
+arguments <- parse_args(parser, positional_arguments = T);
+opt <- arguments$options;
+
+if (is.null(opt$outFile)) {
+    cat("Need output file\n");
+    print_help(parser);
+    stop();
+if (is.null(opt$resultsDir)) {
+    cat("Need results dir\n");
+    print_help(parser);
+    stop();
+} else if (is.null(opt$summaryFile)) {
+    cat("Need summary file\n");
+    print_help(parser);
+    stop();
+} else if (is.null(opt$phasedBGLDir)) {
+    cat("Need phased BGL directory\n");
+    print_help(parser);
+    stop();
+} else if (is.null(opt$callsFile)) {
+    cat("Need calls file\n");
+    print_help(parser);
+    stop();
+} else if (is.null(opt$clustersFile)) {
+    cat("Need clusters file\n");
+    print_help(parser);
+    stop();
+} else if (length(arguments$args) < 1) {
+    cat("Need tumour sample\n");
+    print_help(parser);
+    stop();
+}
+
+tumour <- arguments$args[1]
+if (length(argument$args) > 1) {
+    normal <- argument$args[2]
+    useNormal <- T
 } else {
-	for (i in 1:length(args)) {
-    eval(parse(text = args[[i]]))
-    }
+    useNormal <- F
+    normal <- NULL
 }
 
-if (tumour == "") {
-	print("Need tumour sample name\n")
-    exit(1)
-}
-if (results.dir == "") {
-	print("Need results directory\n")
-    exit(1)
-}
-if (out.file == "") {
-	print("Need output file\n")
-    exit(1)
-}
-if (normal != "") {
-    useNormal = T
-} else {
-    useNormal = F
-}
 
-library(HAPSEG)
-RunHapSeg(out.file = out.file, plate.name = plate.name, array.name = tumour, seg.fn = NULL, 
-	snp.fn = snp.fn, 
-	genome.build = genome.build, results.dir = results.dir,
-	platform = platform, use.pop = "CEPH", impute.gt = TRUE, plot.segfit = TRUE, 
+RunHapSeg(out.file = opt$outFile, plate.name = opt$plate, array.name = tumour, seg.fn = NULL, 
+	snp.fn = opt$summaryFile, 
+	genome.build = opt$ref, results.dir = opt$resultsDir,
+	platform = opt$platform, use.pop = "CEPH", impute.gt = TRUE, plot.segfit = TRUE, 
 	merge.small = TRUE, merge.close = TRUE, min.seg.size = 3, normal = FALSE, out.p = 0.05, 
 	seg.merge.thresh = 0.0001, phased.bgl.dir = phased.bgl, 
 	drop.x = FALSE, drop.y = TRUE, calls.fn = calls.fn , 

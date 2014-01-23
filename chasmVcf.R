1702258be5fa6968ba23e6bf81c35492dd46b60b
raylim <raylim@mm.st>
2014-01-23 13:58:22 -0500

    process chasm vcf in chunks

--- chasmVcf.R
+++ chasmVcf.R
@@ -28,68 +28,78 @@ if (is.null(opt$chasmDir)) {
     print_help(parser);
     stop();
 } else if (length(arguments$args) < 1) {
-    #cat("Reading from stdin...\n");
-    tmp <- tempfile()
-    f <- file('stdin')
-    open(f)
-    x <- readLines(f)
-    close(f)
-    write(x, file = tmp)
-    vcf <- readVcf(tmp, genome = opt$genome)
-} else {
-    fn <- arguments$args[1];
-    vcf <- readVcf(fn, genome = opt$genome)
+    cat("Need vcf file\n");
+    print_help(parser);
+    stop();
 }
 
+fn <- arguments$args[1];
+outfn <- opt$outFile
+out <- file(outfn, open = 'a')
 
-oldwd <- getwd()
+# create new header
+vcfHeader <- scanVcfHeader(fn)
+hinfoprime <- apply(as.data.frame(info(vcfHeader)), 2, as.character)
+rownames(hinfoprime) <- rownames(info(vcfHeader))
+hinfoprime <- rbind(hinfoprime, chasm_mut = c("A", "String", "CHASM mutation"))
+hinfoprime <- rbind(hinfoprime, chasm_pval = c("A", "Float", "CHASM p-value"))
+hinfoprime <- rbind(hinfoprime, chasm_score = c("A", "Float", "CHASM score"))
+hinfoprime <- rbind(hinfoprime, chasm_fdr = c("A", "Float", "CHASM B-H FDR"))
+hinfoprime <- DataFrame(hinfoprime, row.names = rownames(hinfoprime))
+info(vcfHeader) <- hinfoprime
+hlist <- header(vcfHeader)
+hlist$INFO <- hinfoprime
+newVcfHeader <- new("VCFHeader", samples = vcfHeader@samples, header = hlist)
 
-if (sum(rowData(vcf)$FILTER == "PASS") > 0) {
-    # convert vcf to chasm input
-    al <- sapply(rowData(vcf)$ALT, length)
-    X <- data.frame(id = 1:nrow(vcf), seq = as.character(seqnames(rowData(vcf))), zero = as.integer(start(rowData(vcf)) - 1), one = as.integer(start(rowData(vcf))), strand = rep('+', nrow(vcf)), ref = as.character(rowData(vcf)$REF), filt = rowData(vcf)$FILTER, stringsAsFactors = F)
-    re <- rep.int(X$id, times = al)
-    X <- X[re, ,drop = F]
-    X$alt <- as.character(unlist(rowData(vcf)$ALT))
-    X <- subset(X, sapply(alt, nchar) == 1 & sapply(ref, nchar) == 1 & filt == "PASS")
-    X <- X[,-which(colnames(X) == 'filt')]
-    X$seq <- sub('^', 'chr', X$seq)
 
-    setwd(opt$chasmDir)
-    tmp <- tempfile()
-    write.table(X, file = tmp, quote = F, sep = '\t', row.names = F, col.names = F)
-    cmd <- paste("CHASMDIR=", opt$chasmDir, " ", opt$python, ' ./RunChasm ', opt$classifier, ' ', tmp, ' -g' ,sep = '')
-    #cat(cmd)
-    system(cmd, ignore.stdout = T)
-    results <- read.table(file = paste(tmp, opt$classifier, '.output', sep = ''), sep = '\t', header = T, as.is = T)
+temp <- tempfile()
+zipped <- bgzip(fn, temp)
+idx <- indexTabix(temp, "vcf")
+tab <- TabixFile(zipped, idx, yieldSize = 4000)
+open(tab)
+while(nrow(vcf <- readVcf(tab, genome = opt$genome))) {
+    oldwd <- getwd()
 
-    hinfoprime <- apply(as.data.frame(info(header(vcf))), 2, as.character)
-    rownames(hinfoprime) <- rownames(info(header(vcf)))
-    hinfoprime <- rbind(hinfoprime, chasm_mut = c("A", "String", "CHASM mutation"))
-    hinfoprime <- rbind(hinfoprime, chasm_pval = c("A", "Float", "CHASM p-value"))
-    hinfoprime <- rbind(hinfoprime, chasm_score = c("A", "Float", "CHASM score"))
-    hinfoprime <- rbind(hinfoprime, chasm_fdr = c("A", "Float", "CHASM B-H FDR"))
-    hinfoprime <- DataFrame(hinfoprime, row.names = rownames(hinfoprime))
-    hlist <- header(exptData(vcf)$header)
-    hlist$INFO <- hinfoprime
-    exptData(vcf)$header <- new("VCFHeader", samples = header(vcf)@samples, header = hlist)
-}
+    if (sum(rowData(vcf)$FILTER == "PASS") > 0) {
+        # convert vcf to chasm input
+        al <- sapply(rowData(vcf)$ALT, length)
+        X <- data.frame(id = 1:nrow(vcf), seq = as.character(seqnames(rowData(vcf))), zero = as.integer(start(rowData(vcf)) - 1), one = as.integer(start(rowData(vcf))), strand = rep('+', nrow(vcf)), ref = as.character(rowData(vcf)$REF), filt = rowData(vcf)$FILTER, stringsAsFactors = F)
+        re <- rep.int(X$id, times = al)
+        X <- X[re, ,drop = F]
+        X$alt <- as.character(unlist(rowData(vcf)$ALT))
+        X <- subset(X, sapply(alt, nchar) == 1 & sapply(ref, nchar) == 1 & filt == "PASS")
+        X <- X[,-which(colnames(X) == 'filt')]
+        X$seq <- sub('^', 'chr', X$seq)
 
-if (nrow(results) > 1) {
-    infoprime <- info(vcf)
-    infoprime[as.integer(as.character(results$MutationID)),"chasm_mut"] <- as.character(results$Mutation)
-    infoprime[as.integer(as.character(results$MutationID)),"chasm_score"] <- results$CHASM
-    infoprime[as.integer(as.character(results$MutationID)),"chasm_pval"] <- results$PValue
-    if (nrow(results) > 10) {
-        infoprime[as.integer(as.character(results$MutationID)),"chasm_fdr"] <- results$BHFDR
+        setwd(opt$chasmDir)
+        tmp <- tempfile()
+        write.table(X, file = tmp, quote = F, sep = '\t', row.names = F, col.names = F)
+        cmd <- paste("CHASMDIR=", opt$chasmDir, " ", opt$python, ' ./RunChasm ', opt$classifier, ' ', tmp, ' -g' ,sep = '')
+        #cat(cmd)
+        system(cmd, ignore.stdout = T)
+        results <- read.table(file = paste(tmp, opt$classifier, '.output', sep = ''), sep = '\t', header = T, as.is = T)
     }
-    info(vcf) <- infoprime
+
+    exptData(vcf)$header <- newVcfHeader
+
+    if (nrow(results) > 1) {
+        infoprime <- info(vcf)
+        infoprime[as.integer(as.character(results$MutationID)),"chasm_mut"] <- as.character(results$Mutation)
+        infoprime[as.integer(as.character(results$MutationID)),"chasm_score"] <- results$CHASM
+        infoprime[as.integer(as.character(results$MutationID)),"chasm_pval"] <- results$PValue
+        if (nrow(results) > 10) {
+            infoprime[as.integer(as.character(results$MutationID)),"chasm_fdr"] <- results$BHFDR
+        }
+        info(vcf) <- infoprime
+    }
+
+    # fix sample genotype order
+    x <- which(names(geno(vcf)) == "GT")
+    ord <- c(x, (1:length(geno(vcf)))[-x])
+    geno(vcf) <- geno(vcf)[ord]
+
+    setwd(oldwd)
+    writeVcf(vcf, out)
 }
-
-# fix sample genotype order
-x <- which(names(geno(vcf)) == "GT")
-ord <- c(x, (1:length(geno(vcf)))[-x])
-geno(vcf) <- geno(vcf)[ord]
-
-setwd(oldwd)
-writeVcf(vcf, filename = opt$outFile)
+close(tab)
+close(out)

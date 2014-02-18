fc85b5224a47c5099b99d6bb9b640fcfc9057876
raylim <raylim@mm.st>
2014-02-18 17:28:10 -0500

    fixes

--- recurrentFusions.R
+++ recurrentFusions.R
@@ -6,9 +6,9 @@ suppressPackageStartupMessages(library("optparse"));
 options(warn = -1, error = quote({ traceback(); q('no', status = 1) }));
 
 optList <- list(
-                make_option("--geneCol1", default = 1, help = "gene column 1"),
-                make_option("--geneCol2", default = 2, help = "gene column 2"),
-                make_option("--sampleCol", default = 3, help = "sample column"),
+                make_option("--geneCol1", default = NULL, help = "gene column 1"),
+                make_option("--geneCol2", default = NULL, help = "gene column 2"),
+                make_option("--sampleCol", default = NULL, help = "sample column"),
                 make_option("--outDir", default = NULL, help = "Output dir"));
 
 parser <- OptionParser(usage = "%prog [options] fusions_table", option_list = optList);
@@ -19,6 +19,10 @@ opt <- arguments$options;
 
 if (length(arguments$args) != 1) {
     cat("Need input table\n");
+    print_help(parser);
+    stop();
+} else if (is.null(opt$geneCol1) || is.null(opt$geneCol2) || is.null(opt$sampleCol)) {
+    cat("Need column name\n");
     print_help(parser);
     stop();
 } else if (is.null(opt$outDir)) {
@@ -53,7 +57,7 @@ if (sum(x) > 1) {
     oo <- order(rowSums(geneGeneM), decreasing = T)
     recurGeneGeneM <- geneGeneM[oo, oo]
     fn <- paste(opt$outDir, "/recurGeneGene.txt", sep = "")
-    write.table(recurGeneGeneM, file = fn, sep = '\t', quote = F, col.names = NA, row.names = 1)
+    write.table(recurGeneGeneM, file = fn, sep = '\t', quote = F, col.names = NA, row.names = T)
 }
 
 geneM <- sapply(genes, function (x) allGenes %in% x)
@@ -61,10 +65,12 @@ rownames(geneM) <- allGenes
 
 if (sum(rowSums(geneM) > 1) > 1) {
     recurGeneM <- geneM[rowSums(geneM) > 1, ]
-    oo <- order(rowSums(recurGeneM))
+    oo <- order(rowSums(recurGeneM), decreasing = T)
     recurGeneM <- recurGeneM[oo, ]
+    recurGeneM <- ifelse(recurGeneM, 1, 0)
+    recurGeneM <- transform(recurGeneM, Sum = rowSums(recurGeneM))
     fn <- paste(opt$outDir, "/recurGenes.txt", sep = "")
-    write.table(recurGeneM, file = fn, sep = '\t', quote = F, col.names = NA, row.names = 1)
+    write.table(recurGeneM, file = fn, sep = '\t', quote = F, col.names = NA, row.names = T)
 }
 
 genePairs <- list()
@@ -77,10 +83,12 @@ rownames(genePairM) <- allGenePairs
 
 if (sum(rowSums(genePairM) > 1) > 1) {
     recurGenePairM <- genePairM[rowSums(genePairM) > 1, ]
-    oo <- order(rowSums(recurGenePairM))
+    oo <- order(rowSums(recurGenePairM), decreasing = T)
     recurGenePairM <- recurGenePairM[oo, ]
+    recurGenePairM <- ifelse(recurGenePairM, 1, 0)
+    recurGenePairM <- transform(recurGenePairM, Sum = rowSums(recurGenePairM))
     fn <- paste(opt$outDir, "/recurGenePairs.txt", sep = "")
-    write.table(recurGenePairM, file = fn, sep = '\t', quote = F, col.names = NA, row.names = 1)
+    write.table(recurGenePairM, file = fn, sep = '\t', quote = F, col.names = NA, row.names = T)
 }
 
 

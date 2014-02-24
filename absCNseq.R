36a1d65de8837fad468479f6745a4ab9208bebd4
raylim <raylim@mm.st>
2014-02-24 18:56:59 -0500

    update

--- absCNseq.R
+++ absCNseq.R
@@ -1,5 +1,34 @@
+#!/usr/bin/env Rscript
+# applies absCNseq
 
-segData <- read.table(fn, sep = '\t', header = T)
+suppressPackageStartupMessages(library("optparse"));
+suppressPackageStartupMessages(library("absCNseq"));
+
+options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
+
+optList <- list(
+                make_option("--outPrefix", default = NULL, type = "character", action = "store", help ="Output prefix (required)"))
+
+parser <- OptionParser(usage = "%prog [options] varscan.seg.file snv.table", option_list = optList);
+arguments <- parse_args(parser, positional_arguments = T);
+opt <- arguments$options;
+
+if (length(argumetns$args) != 2) {
+    cat("Need varscan seg file and SNV table\n\n");
+    print_help(parser);
+    stop();
+} else if (is.null(opt$outPrefix)) {
+    cat("Need output prefix\n\n");
+    print_help(parser);
+    stop();
+}
+
+segFile <- arguments$args[1]
+snvFile <- arguments$args[2]
+
+tumorName <- sub('\\..*', '', sub('.*/', '', sub('_.*', '', segFile)))
+
+segData <- read.table(segFile, sep = '\t', header = T)
 segRle <- rle(segData$Segmented)
 segData <- transform(segData, segId = as.factor(rep(1:length(segRle$values), segRle$lengths)))
 segData <- transform(segData, length = End - Start)
@@ -11,6 +40,9 @@ end <- tapply(segData$End, segData$segId, function(x) x[length(x)])
 effSegLen <- tapply(segData$length, segData$segId, sum)
 normRatio <- tapply(segData$Segmented, segData$segId, function(x) x[1])
 
-Data <- data.frame(chrom = chrom, loc.start = start, loc.end = end, eff.seg.len = effSegLen, normalized.ratio = normRatio)
+absSegData <- data.frame(chrom = chrom, loc.start = start, loc.end = end, eff.seg.len = effSegLen, normalized.ratio = normRatio)
 
+snvData <- read.table(snvFile, sep = '\t', header = T, comment.char = '')
+af <- paste(tumorName
+absSnvData <- with(snvData, data.frame(chrom = X.CHROM, position = POS, tumor_var_freq = snvData[,af])
 

e0bea2d4b071b35ee444edebce5406acbdd3c36c
raylim <raylim@mm.st>
2014-09-16 13:25:44 -0400

    path

---
+++ backup.sh
@@ -0,0 +1,5 @@
+find data projects -type d \
+    \( -name bam -o -name tables -o -name alltables -o -name vcf \) \
+    ! -path "*/log/*" ! -path "*/tmap/*" ! -path "*/gatk/*" ! -path "*/hydra/*" ! -path "*/bwa/*" \
+    ! -path "*/varscan/*" ! -path "*/mutect/*" ! -path "*/scalpel/*" ! -path "*/som_sniper/*" -print0 | \
+    rsync --verbose --progress -a -0 --files-from=- --prune-empty-dirs ./ /mount/limr/zedshared

fbdd971e907835ad479d7e5dc0cbcec1e4495034
Raymond Lim <raylim@mm.st>
2014-11-10 13:41:53 -0500

    x

--- tableToCloneHDFormat.pl
+++ tableToCloneHDFormat.pl
@@ -30,7 +30,7 @@ while (<STDIN>) {
     my %F = map { $_ => shift @F } @header;
     my $line = "$F{CHROM}\t$F{POS}";
     for my $s (@samples) {
-        my $ad = $F{"$s.AD"};
+        my $ad = $F{$s . ".AD"};
         my @ad = ($ad ne ".")? split /,/, $ad : qw/0 0/;
         $line .= "\t$ad[0]\t" . $ad[0] + $ad[1];
     }

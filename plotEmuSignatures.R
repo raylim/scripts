dc2731d08803ace7d94dca88d14fc1818672b576
Raymond Lim <raylim@mm.st>
2014-11-21 18:19:09 -0500

    emu sig

--- plotEmuSignatures.R
+++ plotEmuSignatures.R
@@ -11,9 +11,10 @@ options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
 
 optList <- list(
                 make_option("--outDir", default = NULL, help = "output dir (required)"),
+                make_option("--mutations", default = NULL, help = "mutations file (required)"),
                 make_option("--samples", default = NULL, help = "samples file"),
                 make_option("--sampleSubset", default = NULL, help = "sample subset file"),
-                make_option("--inPrefix", default = NULL, help = "input prefix (required)"))
+                make_option("--inDir", default = NULL, help = "EMu input directory (required)"))
 
 parser <- OptionParser(usage = "%prog [options]", option_list = optList);
 
@@ -21,24 +22,28 @@ arguments <- parse_args(parser, positional_arguments = T);
 opt <- arguments$options;
 
 if (is.null(opt$outDir)) {
-    cat("Need html output file\n");
+    cat("Need output dir\n");
     print_help(parser);
     stop();
-} else if (is.null(opt$inPrefix)) {
-    cat("Need input prefix\n");
+} else if (is.null(opt$inDir)) {
+    cat("Need EMu input directory\n");
+    print_help(parser);
+    stop();
+} else if (is.null(opt$mutations)) {
+    cat("Need EMu mutations file\n");
     print_help(parser);
     stop();
 } else {
     files <- arguments$args;
 }
 
-glob <- paste(opt$inPrefix, '_*_ml_spectra.txt', sep = '')
+glob <- paste(opt$inDir, '/*_ml_spectra.txt', sep = '')
 spectraFiles <- Sys.glob(glob)
 
-glob <- paste(opt$inPrefix, '_*_map_activities.txt', sep = '')
+glob <- paste(opt$inDir, '/*_map_activities.txt', sep = '')
 activityFiles <- Sys.glob(glob)
 
-glob <- paste(opt$inPrefix, '_*_assigned.txt', sep = '')
+glob <- paste(opt$inDir, '/*_assigned.txt', sep = '')
 assignedFiles <- Sys.glob(glob)
 
 pg <- openPage('index.html', dirname = opt$outDir, title = 'EMu results')
@@ -53,6 +58,7 @@ for (fn in spectraFiles) {
         ofn <- paste(opt$outDir, "/", basename(fn), sep = '')
         ofn <- sub('\\.txt$', paste("_", i, '.pdf', sep = ''), ofn)
         pdf(ofn, height = 5, width = 10)
+        par(cex = 2)
         cols <- rep(c('LightBlue', 'Black', 'Red', 'Grey', 'Green', 'Magenta'), each = 16)
         barplot(t(spectra[i,]) * 100, beside = T, col = cols, border = cols, xaxt = 'n', main = paste("Signature", i), col.main = i, ylab = "% of mutations")
         labs <- c("C>A", "C>G", "C>T", "T>A", "T>C", "T>G")
@@ -63,7 +69,8 @@ for (fn in spectraFiles) {
     for (i in 1:nrow(spectra)) {
         ofn <- paste(opt$outDir, "/", basename(fn), sep = '')
         ofn <- sub('\\.txt$', paste("_", i, '.png', sep = ''), ofn)
-        png(ofn, height = 500, width = 800, type = 'cairo')
+        png(ofn, height = 500, width = 800, type = 'cairo-png')
+        par(cex = 2)
         cols <- rep(c('LightBlue', 'Black', 'Red', 'Grey', 'Green', 'Magenta'), each = 16)
         barplot(t(spectra[i,]) * 100, beside = T, col = cols, border = cols, xaxt = 'n', main = paste("Signature", i), ylab = "% of mutations", col.main = i)
         labs <- c("C>A", "C>G", "C>T", "T>A", "T>C", "T>G")
@@ -73,16 +80,42 @@ for (fn in spectraFiles) {
     }
 }
 
+mutations <- read.table(opt$mutations, sep = ' ')
+colnames(mutations) <- c('sample', 'chr', 'pos', 'snv')
+mutations <- subset(mutations, sample %in% sampleSubset)
 
-samples <- scan('mutations.txt.samples', what = 'character')
+samples <- scan(opt$samples, what = 'character')
 sampleSubset <- scan(opt$sampleSubset, what = 'character')
-x <- which(samples %in% sampleSubset)
 for (fn in assignedFiles) {
     assigned <- read.table(fn, sep = ' ')
     assigned <- as.matrix(assigned[,-ncol(assigned)])
-    assigned <- assigned[x, ]
-    par(mar = c(5, 10, 5, 5))
-    barplot(t(assigned / rowSums(assigned)), col = 1:5, space = 0, border = F, horiz = T, names = samples[x], las = 2)
+    rownames(assigned) <- samples
+    assigned <- assigned[sampleSubset, ]
+
+    ofn <- paste(opt$outDir, "/", basename(fn), sep = '')
+    ofn <- sub('\\.txt$', paste("_", i, '.png', sep = ''), ofn)
+
+    tab <- table(factor(mutations$sample))
+    tab <- tab[sampleSubset]
+    oo <- order(tab)
+    assigned <- assigned[oo, ]
+
+    png(ofn, height = 1000, width = 1000, type = 'cairo-png')
+    par(mar = c(5, 10, 5, 1), cex = 1, mfrow = c(1, 2), cex = 2)
+    barplot(t(assigned / rowSums(assigned)), col = 1:5, space = 0, border = F, horiz = T, las = 2, xlab = "Contribution of signature")
+    par(mar = c(5,1,5,5))
+    barplot(tab[oo], las = 2, horiz = T, space = 0, border = F, xlab = "Number of Mutations", axisnames = F)
+    null <- dev.off()
+    hwriteImage(basename(ofn), pg, br = T)
+
+    ofn <- sub('\\.png$', '.pdf', ofn)
+    pdf(ofn, height = 10, width = 10)
+    par(mar = c(5, 10, 5, 1), cex = 1, mfrow = c(1, 2), cex = 2)
+    barplot(t(assigned / rowSums(assigned)), col = 1:5, space = 0, border = F, horiz = T, las = 2, xlab = "Contribution of signature")
+    par(mar = c(5,1,5,5))
+    barplot(tab[oo], las = 2, horiz = T, space = 0, border = F, xlab = "Number of Mutations", axisnames = F)
+    null <- dev.off()
 }
+
 
 closePage(pg)

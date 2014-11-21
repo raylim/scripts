2679b30af4cb266b2afe79e99feca06a9ba229c3
Raymond Lim <raylim@mm.st>
2014-11-20 19:30:26 -0500

    mutations

--- plotEmuSignatures.R
+++ plotEmuSignatures.R
@@ -4,13 +4,15 @@
 suppressPackageStartupMessages(library("optparse"));
 suppressPackageStartupMessages(library("VariantAnnotation"));
 suppressPackageStartupMessages(library("RColorBrewer"));
+suppressPackageStartupMessages(library("hwriter"));
 suppressPackageStartupMessages(library("org.Hs.eg.db"));
 
 options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
 
 optList <- list(
-                make_option("--outPrefix", default = NULL, help = "Output prefix (required)"),
-                make_option("--samples", default = NULL, help = "samples file; labels axes if specified"),
+                make_option("--outDir", default = NULL, help = "output dir (required)"),
+                make_option("--samples", default = NULL, help = "samples file"),
+                make_option("--sampleSubset", default = NULL, help = "sample subset file"),
                 make_option("--inPrefix", default = NULL, help = "input prefix (required)"))
 
 parser <- OptionParser(usage = "%prog [options]", option_list = optList);
@@ -18,8 +20,8 @@ parser <- OptionParser(usage = "%prog [options]", option_list = optList);
 arguments <- parse_args(parser, positional_arguments = T);
 opt <- arguments$options;
 
-if (is.null(opt$outPrefix)) {
-    cat("Need output prefix\n");
+if (is.null(opt$outDir)) {
+    cat("Need html output file\n");
     print_help(parser);
     stop();
 } else if (is.null(opt$inPrefix)) {
@@ -38,3 +40,49 @@ activityFiles <- Sys.glob(glob)
 
 glob <- paste(opt$inPrefix, '_*_assigned.txt', sep = '')
 assignedFiles <- Sys.glob(glob)
+
+pg <- openPage('index.html', dirname = opt$outDir, title = 'EMu results')
+
+set.seed(001)
+palette(sample(rainbow(30)))
+
+for (fn in spectraFiles) {
+    spectra <- read.table(fn, sep = ' ')
+    spectra <- spectra[,-97] # remove empty col
+    for (i in 1:nrow(spectra)) {
+        ofn <- paste(opt$outDir, "/", basename(fn), sep = '')
+        ofn <- sub('\\.txt$', paste("_", i, '.pdf', sep = ''), ofn)
+        pdf(ofn, height = 5, width = 10)
+        cols <- rep(c('LightBlue', 'Black', 'Red', 'Grey', 'Green', 'Magenta'), each = 16)
+        barplot(t(spectra[i,]) * 100, beside = T, col = cols, border = cols, xaxt = 'n', main = paste("Signature", i), col.main = i, ylab = "% of mutations")
+        labs <- c("C>A", "C>G", "C>T", "T>A", "T>C", "T>G")
+        mtext(labs, side = 1, at = 1:6 * 16 - 7.5)
+        null <- dev.off()
+    }
+
+    for (i in 1:nrow(spectra)) {
+        ofn <- paste(opt$outDir, "/", basename(fn), sep = '')
+        ofn <- sub('\\.txt$', paste("_", i, '.png', sep = ''), ofn)
+        png(ofn, height = 500, width = 800, type = 'cairo')
+        cols <- rep(c('LightBlue', 'Black', 'Red', 'Grey', 'Green', 'Magenta'), each = 16)
+        barplot(t(spectra[i,]) * 100, beside = T, col = cols, border = cols, xaxt = 'n', main = paste("Signature", i), ylab = "% of mutations", col.main = i)
+        labs <- c("C>A", "C>G", "C>T", "T>A", "T>C", "T>G")
+        mtext(labs, side = 1, at = 1:6 * 16 - 7.5)
+        null <- dev.off()
+        hwriteImage(basename(ofn), pg, br = T)
+    }
+}
+
+
+samples <- scan('mutations.txt.samples', what = 'character')
+sampleSubset <- scan(opt$sampleSubset, what = 'character')
+x <- which(samples %in% sampleSubset)
+for (fn in assignedFiles) {
+    assigned <- read.table(fn, sep = ' ')
+    assigned <- as.matrix(assigned[,-ncol(assigned)])
+    assigned <- assigned[x, ]
+    par(mar = c(5, 10, 5, 5))
+    barplot(t(assigned / rowSums(assigned)), col = 1:5, space = 0, border = F, horiz = T, names = samples[x], las = 2)
+}
+
+closePage(pg)

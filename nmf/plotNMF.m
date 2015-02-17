aaf96ef63ba822f1392aa5557087b47fefa595c0
Raymond Lim <raylim@mm.st>
2015-02-17 14:07:18 -0500

    str

--- nmf/plotNMF.m
+++ nmf/plotNMF.m
@@ -4,6 +4,9 @@ addpath(strcat(nmfDir, '/source/'));
 addpath(strcat(nmfDir, '/plotting/'));
 mkdir('temp');
 
+minNumSig = str2num(minNumSig);
+maxNumSig = str2num(maxNumSig);
+
 for totalSignatures = minNumSig : maxNumSig
     inputFile = strcat(prefix, '_ts', num2str(totalSignatures), '.mat');
     load(inputFile);

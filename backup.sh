c0633a61400c9fc18e1c599e922a17e513061341
Raymond Lim <raylim@mm.st>
2015-04-02 12:28:07 -0400

    use dir for lock

--- backup.sh
+++ backup.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-PID=~/.backup.pid
+LOCK=~/.backup_lock
+if ! mkdir $LOCK 2> /dev/null; then
+    echo "backup script is already running"
+    exit 1
+fi
+
 TMP=`mktemp`;
 if mountpoint -q "/mount/limr/zedshared/"; then
-    if [[ -s $PID ]] || [[ -z `pgrep -F $PID` ]]; then 
-        echo "searching for files in /ifs/e63data/reis-filho/"
-        cd /ifs/e63data/reis-filho/ && \
-        find data projects -type d \
-            \( -name bam -o -name tables -o -name alltables -o -name vcf \) \
-            ! -path "*/log/*" ! -path "*/tmap/*" ! -path "*/gatk/*" ! -path "*/hydra/*" ! -path "*/bwa/*" \
-            ! -path "*/varscan/*" ! -path "*/mutect/*" ! -path "*/scalpel/*" ! -path "*/som_sniper/*" ! -path "*/rawdata/*" \
-            ! -path "*/unprocessed_bam/*" ! -path "*/defuse/*" ! -path "*/chimscan/*" -print0 > ${TMP} &
-        echo $! > $PID
-        wait
+    echo "searching for files in /ifs/e63data/reis-filho/"
+    cd /ifs/e63data/reis-filho/ && \
+    find data projects -type d \
+        \( -name bam -o -name tables -o -name alltables -o -name vcf \) \
+        ! -path "*/log/*" ! -path "*/tmap/*" ! -path "*/gatk/*" ! -path "*/hydra/*" ! -path "*/bwa/*" \
+        ! -path "*/varscan/*" ! -path "*/mutect/*" ! -path "*/scalpel/*" ! -path "*/som_sniper/*" ! -path "*/rawdata/*" \
+        ! -path "*/unprocessed_bam/*" ! -path "*/defuse/*" ! -path "*/chimscan/*" -print0 > ${TMP}
 
-        while [ 1 ]; do
-            cd /ifs/e63data/reis-filho/ && \
-            rsync --verbose --checksum --progress --stats --recursive --append --partial -a -0 --files-from=${TMP} --prune-empty-dirs ./ /mount/limr/zedshared &
-            echo $! > $PID
-            wait
-            if [ "$?" = "0" ]; then
-                echo "rsync complete"
-                exit
-            else
-                echo "rsync failure, retrying in 1 minute..."
-                sleep 60
-            fi
-        done
-        rm $PID
-        rm ${TMP}
-    fi
+    while [ 1 ]; do
+        cd /ifs/e63data/reis-filho/ && \
+        rsync --verbose --checksum --progress --stats --recursive --append --partial -a -0 --files-from=${TMP} --prune-empty-dirs ./ /mount/limr/zedshared
+        if [ "$?" = "0" ]; then
+            echo "rsync complete"
+            exit
+        else
+            echo "rsync failure, retrying in 1 minute..."
+            sleep 60
+        fi
+    done
+    rm ${TMP}
 fi
+
+rmdir $LOCK

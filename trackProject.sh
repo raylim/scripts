a6b2e2124524d89beda20d270b5fbc81e627444d
raylim <raylim@mm.st>
2015-05-05 15:54:13 -0400

    track projects with git

---
+++ trackProject.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+find -ls > fls.txt 
+git add fls.txt
+
+git add sample_sets.txt
+git add samples.txt
+stat -t *.bed &> /dev/null && git add *.bed
+stat -t alltables/*.txt &> /dev/null && git add alltables/*.txt
+stat -t metrics/* &> /dev/null && git add metrics/*
+stat -t version/* &> /dev/null && git add version/*
+
+git commit -m "$(date) : project update"
+git push

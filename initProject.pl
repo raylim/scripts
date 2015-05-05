f480b90955d73a848a52bf4652edf4d2a426fe1a
Raymond Lim <raylim@mm.st>
2015-05-05 14:25:27 -0400

    initialize git repo

--- initProject.pl
+++ initProject.pl
@@ -1,5 +1,19 @@
 #!/usr/bin/env perl
 
+use Cwd;
+
+# create repo
+my $repoName = getcwd;
+$repoName =~ s:.*/projects/::;
+$repoName =~ s:.*/data/::;
+$repoName =~ s:.*kinglab/::;
+$repoName =~ s:/:_:g;
+
+system "bb create -c --owner jrflab --protocol ssh $repoName";
+system "git init";
+system "git remote add git\@bitbucket.org:jrflab/$repoName.git";
+
+
 my $MAKEFILE = <<ENDL;
 export REF = hg19
 DUP_TYPE = markdup
@@ -15,9 +29,18 @@ QSUB_PRIORITY = -800
 include modules/Makefile
 ENDL
 
-system "git clone https://github.com/raylim/scripts.git";
-system "git clone https://github.com/raylim/modules.git";
+unless (-d "scripts") {
+    system "git clone https://github.com/raylim/scripts.git";
+}
+unless (-d "modules") {
+    system "git clone https://github.com/raylim/modules.git";
+}
 
-open OUT, ">Makefile";
-print OUT $MAKEFILE;
+unless (-e "Makefile") {
+    open OUT, ">Makefile";
+    print OUT $MAKEFILE;
+}
+system "git add Makefile";
+system "git commit -m 'makefile'";
+system "git push";
 
